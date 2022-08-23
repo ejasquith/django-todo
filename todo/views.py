7dc6f65286363d61342d736678bd858033ad5be6
ejasquith <emily.asquith@icloud.com>
2022-08-23 10:28:29 +0000

    Create Item model

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
